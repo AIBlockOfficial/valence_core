1b4ac0edc6827f6e40802b5590f3e3dd93f8f426
BHouwens <zhion360@gmail.com>
2023-11-23 12:00:51 +0100

    Error handling for public key and signature encoding

--- src/utils.rs
+++ src/utils.rs
@@ -10,13 +10,21 @@ use serde::{Deserialize, Serialize};
 /// * `msg` - The message that was signed
 /// * `signature` - The signature of the message
 pub fn validate_signature(public_key: &str, msg: &str, signature: &str) -> bool {
-    let pk_decode = hex::decode(public_key).expect("Decoding failed");
-    let sig_decode = hex::decode(signature).expect("Decoding failed");
+    let pk_decode = hex::decode(public_key);
+    let sig_decode = hex::decode(signature);
 
-    let pk = PublicKey::from_slice(&pk_decode).unwrap();
-    let signature = Signature::from_slice(&sig_decode).unwrap();
+    if pk_decode.is_err() || sig_decode.is_err() {
+        return false;
+    }
 
-    sign::verify_detached(&signature, msg.as_bytes(), &pk)
+    let pk = PublicKey::from_slice(&pk_decode.unwrap());
+    let signature = Signature::from_slice(&sig_decode.unwrap());
+
+    if pk.is_none() || signature.is_none() {
+        return false;
+    }
+
+    sign::verify_detached(&signature.unwrap(), msg.as_bytes(), &pk.unwrap())
 }
 
 /// Function to serialize data
